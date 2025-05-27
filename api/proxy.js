d147b6847d08370bd771b9821cf6ae3ebe639d3f
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-05-27 14:11:51 +0900

    [250527] Update: vercel에 배포 후 생성된 URL을 proxyURL 변수에 삽입

--- api/proxy.js
+++ api/proxy.js
@@ -16,7 +16,8 @@ module.exports = async (req, res) => {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Encoding': ''
+        'Accept-Encoding': '',
+        'Connection': 'close'
       },
     });
 
